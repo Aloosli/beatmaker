32b64ce7d8bf55f50ea3f662ac3b8db25caede9d
aloosli <lizzwizz@tuta.io>
2023-02-11 18:54:08 +0000

    add eventListeners

--- app.js
+++ app.js
@@ -126,3 +126,34 @@ class DrumKit {
     }
   }
 }
+//Event Listeners
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+
+drumKit.playBtn.addEventListener("click", function () {
+  drumKit.updateBtn();
+  drumKit.start();
+});
+
+drumKit.selects.forEach((select) => {
+  select.addEventListener("change", function (e) {
+    drumKit.changeSound(e);
+  });
+});
+drumKit.muteBtns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    drumKit.mute(e);
+  });
+});
+
+drumKit.tempoSlider.addEventListener("input", function (e) {
+  drumKit.changeTempo(e);
+});
+drumKit.tempoSlider.addEventListener("change", function (e) {
+  drumKit.updateTempo(e);
+});
